515a7e61f5c1415adfa7deb2ede400b2cd9518ae
Ferew Tucho <ferewtucho@gmail.com>
2019-05-12 12:32:12 -0400

    testing get by id and find mongoose models

--- src/index.js
+++ src/index.js
@@ -23,7 +23,33 @@ app.post("/users", (req, res) => {
     });
 });
 
-app.post("/tasks", (req, res) => {
+app.get("/users", (req, res) => {
+  User.find({})
+    .then(users => {
+      res.send(users);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send();
+      }
+
+      res.send(user);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+app.post("/task", (req, res) => {
   const task = new Task(req.body);
 
   task
@@ -33,6 +59,32 @@ app.post("/tasks", (req, res) => {
     })
     .catch(e => {
       res.status(400).send(e);
+    });
+});
+
+app.get("/task", (req, res) => {
+  Task.find({})
+    .then(task => {
+      res.send(task);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+app.get("/task/:id", (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then(task => {
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch(e => {
+      res.status(500).send();
     });
 });
 
